3342ffd596146c1a6e372af8ba0f06f9c5b9b3c8
NikkiDonthe1 <nikkidonmusic@gmail.com>
2025-02-10 21:23:14 -0800

    Updated homepage content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to WBE3Music! 🎵</h1>
+      <p>This is the official platform for WBE3Music, bringing exclusive music, virtual shows, and more!</p>
     </div>
   );
 }
-
-export default App;
