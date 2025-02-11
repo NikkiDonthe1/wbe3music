d40c2a04f15005412d3bf02cf44a9a4e456aebcd
NikkiDonthe1 <nikkidonmusic@gmail.com>
2025-02-10 22:28:58 -0800

    Added basic styling and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 function App() {
-    return (
-        <div>
-            <h1>Welcome to WBE3Music</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to WBE3Music</h1>
+      <p>More content coming soon...</p>
+    </div>
+  );
 }
 
-export default App;  // <-- This line is important!
+export default App;
+import './App.css';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to WBE3Music</h1>
+      <p>More content coming soon...</p>
+      <button onClick={() => alert("Stay tuned!")}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
